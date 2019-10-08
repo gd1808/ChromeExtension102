823b5227a2dbdbdf6562407857a9849707773ca3
Andrew Lee <axl8175@rit.edu>
2019-10-08 15:07:44 -0400

    Initial written code for user creating their account

--- src/model/User.java
+++ src/model/User.java
@@ -1,25 +1,61 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class User {
     private String username;
     private String password;
+    private HashMap<String, String > hashMap;
 
     public User(String username, String password){
         this.username = username;
         this.password = password;
+        this.hashMap = new HashMap<>();
+
+        addUser(username, password);
+    }
+
+    public boolean addUser(String username, String password) {
+        if(isValid(username)){
+            hashMap.put(username, password);
+            return true;
+        }
+        else{
+            System.out.println("You done goofed");
+            return false;
+        }
     }
 
     public String getUser(){
         return this.username;
     }
 
-    public boolean isValid(){
+    public boolean isValid(String username){
         return false;
     }
 
-    public List<User> Users(){
-        return null;
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
     }
 }
