f02501f6a3ea1ecf96c95ce330a6123ce800dbd1
Mason Zhong <mz9865@rit.edu>
2019-10-08 14:46:39 -0400

    Implemented partial code for Scraper database

--- src/model/ScraperDB.java
+++ src/model/ScraperDB.java
@@ -1,4 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ScraperDB {
+
+    private String key_word;
+    private ArrayList<String> emails;
+
+    public ScraperDB (String key_word) {
+        this.key_word = key_word;
+    }
+
+    public void store (String email) {
+        emails.add(email);
+    }
+
 }
