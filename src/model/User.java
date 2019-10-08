7c1507ca6de9f5766cdafedb60f8f39f52a95c94
Andrew Lee <axl8175@rit.edu>
2019-10-08 14:44:13 -0400

    Skeletong coded User and UserInterface from the initial UML Diagram

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.List;
+
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUser(){
+        return this.username;
+    }
+
+    public boolean isValid(){
+        return false;
+    }
+
+    public List<User> Users(){
+        return null;
+    }
 }
