1ee93fb0e0370a444e6ac04c3f4cdbd31658c6b3
Andrew Lee <axl8175@rit.edu>
2019-10-08 15:15:57 -0400

    Creating fields that are able to store the email account

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -1,7 +1,11 @@
 package ui;
 
+import model.*;
+
 public class UserInterface {
     private String keyWord;
+    private String email_username;
+    private String email_password;
 
     public boolean setKeyWord(String word){
         return false;
