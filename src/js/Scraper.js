c752ad540b4b2e27651fa242423affc4dca6239c
Gabriel Diaz <gd1808@rit.edu>
2019-10-08 15:22:59 -0400

    proof of concept will delete later for actual code

--- src/js/Scraper.js
+++ src/js/Scraper.js
@@ -1,1 +1,16 @@
 import cheerio from 'cheerio';
+
+//let $ = cheerio.load('');
+
+let getData = html => {
+    var data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+        data.push({
+            title : $(elem).text(),
+            link : $(elem).find('a.storylink').attr('href')
+        });
+    });
+    console.log(data);
+}
+getData(response.data)
